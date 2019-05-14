305fce7773cfdb11b84b401307480d3b1572fbcb
CastilloLuis <luisedcastillog@gmail.com>
2019-05-13 21:34:51 -0400

    adding firebase config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import Vue from 'vue';
+import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBmso8tDteOWAe4I5-l7WCYejfvC5ofDdc',
+  authDomain: 'findrooms-32423.firebaseapp.com',
+  databaseURL: 'https://findrooms-32423.firebaseio.com',
+  projectId: 'findrooms-32423',
+  storageBucket: 'findrooms-32423.appspot.com',
+  messagingSenderId: '1074730093945',
+  appId: '1:1074730093945:web:1e1eecf07e86b7a8',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
